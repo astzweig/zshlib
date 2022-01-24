6ff9e9ac8e4eb87165633357a06d915a35413d73
Rezart Qelibari <entwickler-kontakt@astzweig.de>
2022-01-24 02:48:03 +0100

    Add config

--- README.md
+++ README.md
@@ -21,9 +21,14 @@ in your script and you are ready to go
 | ---- | ----------- | ------------ | ------------------- |
 | hio  | Highlighted output to command line. | [`docopts`][docopts][^docopts] | all |
 | getPrefDir | Get system specific preferences directory. | - | macOS, Linux, Windows Subsystem for Linux |
+| config | Config file writer and reader. | `getPrefDir`[^zshlib], `PlistBuddy`[^plistbuddy] | macOS, Linux, Windows Subsystem for Linux |
 
+[^zshlib]: A command of Zsh Lib (this library).
 [^docopts]: `docopts` with `-f, --function` option needed. See the
   [Astzweig fork][astzweig-docopts] for example.
+[^plistbuddy]: `/usr/libexec/PlistBuddy` is a utility found on macOS systems.
+  Apple does not provide any documentation besides the man page and the programs
+  `--help` output.
 
 [git-submodule]: https://git-scm.com/docs/git-submodule
 [docopts]: https://github.com/docopt/docopts
---
+++ spec/config_spec.sh
@@ -0,0 +1,86 @@
+% APPNAME: 'com.example.zshlib'
+% KEYVALUE: 'myvalue'
+
+config_does_not_exist() {
+  config_does_exist && return 12 || return 0
+}
+
+config_does_exist() {
+  test -f "${HOME}/Library/Preferences/${APPNAME}.plist"
+}
+
+no_array_in_config() {
+  grep 'array' "${HOME}/Library/Preferences/${APPNAME}.plist" || return 0
+}
+
+array_in_config() {
+  grep 'array' "${HOME}/Library/Preferences/${APPNAME}.plist" > /dev/null
+}
+
+Describe 'config'
+  setup() { 
+    config_does_not_exist
+    config setappname "${APPNAME}"
+    config write "${KEYVALUE}" someexistentkey
+    config write "${KEYVALUE}" some existent key
+  }
+  cleanup() {
+    local file="${HOME}/Library/Preferences/${APPNAME}.plist"
+    [ -f "${file}" ] && rm "${file}"
+    return 0
+  }
+  BeforeEach 'setup'
+  AfterEach 'cleanup'
+
+  It 'prints usage when no argument is given'
+    When call config
+    The output should eq ''
+    The error should match pattern 'error:*Usage:*config*'
+    The status should eq 64
+  End
+
+  It 'exits cleanly when setting the app name'
+    When call config setappname "${APPNAME}"
+    The output should eq ''
+    The status should be success
+  End
+
+  It 'creates config on first write'
+    When call config write myval mykey
+    Assert config_does_exist
+    The output should eq ''
+    The status should be success
+  End
+
+  It 'returns nothing for nonexistent key'
+    When call config read mykey
+    The output should eq ''
+    The status should be failure
+  End
+
+  It 'returns something for existent key'
+    When call config read someexistentkey
+    The output should eq "${KEYVALUE}"
+    The status should be success
+  End
+
+  It 'creates string key when first key is integer'
+    When call config write myval 0
+    Assert no_array_in_config
+    The output should eq ''
+    The status should be success
+  End
+
+  It 'creates array when key is integer'
+    When call config write myval mykey 0
+    Assert array_in_config
+    The output should eq ''
+    The status should be success
+  End
+
+  It 'reads nested keys'
+    When call config read some existent key
+    The output should eq "${KEYVALUE}"
+    The status should be success
+  End
+End
