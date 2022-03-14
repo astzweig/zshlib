dea9d524bae79fa1ffa1380d8b98ca2a5e5d728b
Rezart Qelibari <entwickler-kontakt@astzweig.de>
2022-03-14 04:46:55 +0100

    Do not store passwords in log by default

--- spec/askUserModuleQuestions_answerQuestionsFromCacheOrAskUser_spec.sh
+++ spec/askUserModuleQuestions_answerQuestionsFromCacheOrAskUser_spec.sh
@@ -76,4 +76,33 @@ Describe 'askUserModuleQuestions answerQuestionsFromCacheOrAskUser'
     The variable writtenValue should eq ''
     The status should be success
   End
+
+  It 'does not store the user answer in cache if question for password'
+    declare -A answers
+    declare -A questions=([some-password]=$'What is your login password?\ninfo')
+    writtenValue=""
+    Data 'red'
+    config() { [ "${1}" = read ] && return; [ "${1}" = write ] && writtenValue="${2}" }
+    cache=config
+    When call _zshlib_askUserModuleQuestions_answerQuestionsFromCacheOrAskUser
+    The output should eq "➔ What is your login password? "
+    The variable "answers[${mod}_some-password]" should eq 'red'
+    The variable writtenValue should eq ''
+    The status should be success
+  End
+
+  It 'does store the user answer in cache if question for password and -p option given'
+    declare -A answers
+    declare -A questions=([some-password]=$'What is your login password?\ninfo')
+    writtenValue=""
+    Data 'red'
+    config() { [ "${1}" = read ] && return; [ "${1}" = write ] && writtenValue="${2}" }
+    cache=config
+    save_password=true
+    When call _zshlib_askUserModuleQuestions_answerQuestionsFromCacheOrAskUser
+    The output should eq "➔ What is your login password? "
+    The variable "answers[${mod}_some-password]" should eq 'red'
+    The variable writtenValue should eq 'red'
+    The status should be success
+  End
 End
