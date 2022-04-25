5cd558419fadef4e78c9a6d1dd38a88f6b2e846d
Rezart Qelibari <entwickler-kontakt@astzweig.de>
2022-04-25 02:06:08 +0200

    Parse terminal answer using positional information
    Parsing the terminal answer produced sometimes a rgb: prefixed string
    most probably because of special chars in the string.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,7 +5,9 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
-Nothing here yet.
+## Fixed
+- Changed parsing semantics of isTerminalBackgroundDark to handle special chars
+  in Terminal answer to xterm control sequence.
 
 ## [v1.0.0] - 2014-05-31
 First major release.
--- spec/isTerminalBackgroundDark_spec.sh
+++ spec/isTerminalBackgroundDark_spec.sh
@@ -10,21 +10,21 @@ Describe 'isTerminalBackgroundDark'
 
   It 'returns failure if color is white'
     print() {}
-    Data '1;rgb:ffff/ffff/ffff'
+    Data '->11;rgb:ffff/ffff/ffff'
     When call isTerminalBackgroundDark
     The status should be failure
   End
 
   It 'returns success if color is black'
     print() {}
-    Data '1;rgb:0000/0000/0000'
+    Data '->11;rgb:0000/0000/0000'
     When call isTerminalBackgroundDark
     The status should be success
   End
 
   It 'returns success if color is dark blue'
     print() {}
-    Data '11;rgb:2b5f/66a6/c977'
+    Data '->11;rgb:2b5f/66a6/c977'
     When call isTerminalBackgroundDark
     The status should be success
   End
